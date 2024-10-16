26d17b7aa458bc93645724d3259a2060083da583
Philipp Born <git@pborn.eu>
2024-10-16 18:53:45 +0200

    hack/bump.sh: bump chart major/minor/patch based on appVersion major/minor/patch

--- hack/bump.sh
+++ hack/bump.sh
@@ -4,23 +4,45 @@ for f in charts/*/Chart.yaml
 do
   git diff --exit-code $(dirname $f) && continue
 
-  if [[ -f $f ]]
-  then
-    current_version=$( grep "^version:" ${f} | sed 's/[^0-9\.]//g' )
-    if [[ ! -z ${current_version} ]]
-    then
-      slice=( ${current_version//./ } )                   # replace points, split into array
-      ((slice[1]++))                                      # increment minor
-      new_version="${slice[0]}.${slice[1]}.${slice[2]}"   # compose new version
-      echo "Bumping ${f}: ${current_version} -> ${new_version}"
+  # fetch old and new appVersion
+  old_version=$( git diff ${f} | grep '^-' | grep 'appVersion' | cut -d' ' -f2 | xargs echo )
+  new_version=$( git diff ${f} | grep '^+' | grep 'appVersion' | cut -d' ' -f2 | xargs echo )
 
-      if [[ "${OSTYPE}" == "darwin"* ]]
-      then
-        # sed -i '' "s|${current_version}|${new_version}|" "${f}"
-        sed -i '' '/^version/ s/'${current_version}'/'${new_version}'/' ${f}
-      else
-        sed -i "s|${current_version}|${new_version}|" "${f}"
-      fi
+  if [[ -z "$old_version" || -z "$new_version" ]]; then
+    echo "appVersion not found in the diff"
+    exit 1
+  fi
+
+  # Split versions into major, minor, and patch
+  IFS='.' read -r old_major old_minor old_patch <<< "$old_version"
+  IFS='.' read -r new_major new_minor new_patch <<< "$new_version"
+
+  current_version=$( grep "^version:" ${f} | sed 's/[^0-9\.]//g' )
+  if [[ ! -z ${current_version} ]]
+  then
+    IFS='.' read -r major minor patch <<< "${current_version}"
+
+    if [[ "$new_major" -gt "$old_major" ]]; then
+      ((major++))
+      minor=0
+      patch=0
+    elif [[ "$new_minor" -gt "$old_minor" ]]; then
+      ((minor++))
+      patch=0
+    elif [[ "$new_patch" -gt "$old_patch" ]]; then
+      ((patch++))
+    fi
+
+    # compose new version
+    new_version="${major}.${minor}.${patch}"
+    echo "Bumping ${f}: ${current_version} -> ${new_version}"
+
+    if [[ "${OSTYPE}" == "darwin"* ]]
+    then
+      # sed -i '' "s|${current_version}|${new_version}|" "${f}"
+      sed -i '' '/^version/ s/'${current_version}'/'${new_version}'/' ${f}
+    else
+      sed -i "s|${current_version}|${new_version}|" "${f}"
     fi
   fi
 done
