375cc068b137fc57a83606cd05ce68ad534a52ea
Philipp Born <git@pborn.eu>
2024-10-16 19:02:49 +0200

    hack/bump.sh: default to patch version bumping, if something else was modified

--- hack/bump.sh
+++ hack/bump.sh
@@ -8,28 +8,32 @@ do
   old_version=$( git diff ${f} | grep '^-' | grep 'appVersion' | cut -d' ' -f2 | xargs echo )
   new_version=$( git diff ${f} | grep '^+' | grep 'appVersion' | cut -d' ' -f2 | xargs echo )
 
-  if [[ -z "$old_version" || -z "$new_version" ]]; then
+  if [[ -z "${old_version}" || -z "${new_version}" ]]; then
     echo "appVersion not found in the diff"
-    exit 1
+    old_version=0.0.0
+    new_version=0.0.1
   fi
 
   # Split versions into major, minor, and patch
-  IFS='.' read -r old_major old_minor old_patch <<< "$old_version"
-  IFS='.' read -r new_major new_minor new_patch <<< "$new_version"
+  IFS='.' read -r old_major old_minor old_patch <<< "${old_version}"
+  IFS='.' read -r new_major new_minor new_patch <<< "${new_version}"
 
   current_version=$( grep "^version:" ${f} | sed 's/[^0-9\.]//g' )
   if [[ ! -z ${current_version} ]]
   then
     IFS='.' read -r major minor patch <<< "${current_version}"
 
-    if [[ "$new_major" -gt "$old_major" ]]; then
+    if [[ "${new_major}" -gt "${old_major}" ]]
+    then
       ((major++))
       minor=0
       patch=0
-    elif [[ "$new_minor" -gt "$old_minor" ]]; then
+    elif [[ "${new_minor}" -gt "${old_minor}" ]]
+    then
       ((minor++))
       patch=0
-    elif [[ "$new_patch" -gt "$old_patch" ]]; then
+    elif [[ "${new_patch}" -gt "${old_patch}" ]]
+    then
       ((patch++))
     fi
 
